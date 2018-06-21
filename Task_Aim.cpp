851df816b4f43a1e792c68a714827a4f649a8e67
streamclass <streamclass18@gmail.com>
2018-06-21 16:43:29 +0900

    2018/06/21

---
+++ Task_Aim.cpp
@@ -0,0 +1,130 @@
+//-------------------------------------------------------------------
+//タイトル画面
+//-------------------------------------------------------------------
+#include  "MyPG.h"
+#include  "Task_Aim.h"
+#include  "Task_Player.h"
+
+namespace  Aiming
+{
+	Resource::WP  Resource::instance;
+	//-------------------------------------------------------------------
+	//リソースの初期化
+	bool  Resource::Initialize()
+	{
+		this->imageName = "AimImg";
+		DG::Image_Create(this->imageName, "./data/image/AimImg.png");
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//リソースの解放
+	bool  Resource::Finalize()
+	{
+		DG::Image_Erase(this->imageName);
+		return true;
+	}
+	//-------------------------------------------------------------------
+	//「初期化」タスク生成時に１回だけ行う処理
+	bool  Object::Initialize()
+	{
+		//スーパークラス初期化
+		__super::Initialize(defGroupName, defName, true);
+		//リソースクラス生成orリソース共有
+		this->res = Resource::Create();
+
+		//★データ初期化
+		this->render2D_Priority[1] = 0.2f;
+		this->hitBase = ML::Box3D(0, 0, 0, 100, 10, 10);
+		//★タスクの生成
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「終了」タスク消滅時に１回だけ行う処理
+	bool  Object::Finalize()
+	{
+		//★データ＆タスク解放
+
+
+		if (!ge->QuitFlag() && this->nextTaskCreate)
+		{
+			//★引き継ぎタスクの生成
+		}
+
+		return  true;
+	}
+	//-------------------------------------------------------------------
+	//「更新」１フレーム毎に行う処理
+	void  Object::UpDate()
+	{
+		ge->GetTask_One_G<Player::Object>("プレイヤ");
+		//プレイヤの向きに合わせて当たり判定矩形の向きを変更
+
+	}
+	//-------------------------------------------------------------------
+	//「２Ｄ描画」１フレーム毎に行う処理
+	void  Object::Render2D_AF()
+	{
+		ML::Box2D draw(0, 0, 1920, 1080);
+		ML::Box2D src = draw;
+		DG::Image_Draw(this->res->imageName, draw, src);
+	}
+
+	void  Object::Render3D_L0()
+	{
+	}
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//以下は基本的に変更不要なメソッド
+	//★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	//-------------------------------------------------------------------
+	//タスク生成窓口
+	Object::SP  Object::Create(bool  flagGameEnginePushBack_)
+	{
+		Object::SP  ob = Object::SP(new  Object());
+		if (ob) {
+			ob->me = ob;
+			if (flagGameEnginePushBack_) {
+				ge->PushBack(ob);//ゲームエンジンに登録
+			}
+			if (!ob->B_Initialize()) {
+				ob->Kill();//イニシャライズに失敗したらKill
+			}
+			return  ob;
+		}
+		return nullptr;
+	}
+	//-------------------------------------------------------------------
+	bool  Object::B_Initialize()
+	{
+		return  this->Initialize();
+	}
+	//-------------------------------------------------------------------
+	Object::~Object() { this->B_Finalize(); }
+	bool  Object::B_Finalize()
+	{
+		auto  rtv = this->Finalize();
+		return  rtv;
+	}
+	//-------------------------------------------------------------------
+	Object::Object() {	}
+	//-------------------------------------------------------------------
+	//リソースクラスの生成
+	Resource::SP  Resource::Create()
+	{
+		if (auto sp = instance.lock()) {
+			return sp;
+		}
+		else {
+			sp = Resource::SP(new  Resource());
+			if (sp) {
+				sp->Initialize();
+				instance = sp;
+			}
+			return sp;
+		}
+	}
+	//-------------------------------------------------------------------
+	Resource::Resource() {}
+	//-------------------------------------------------------------------
+	Resource::~Resource() { this->Finalize(); }
+}
